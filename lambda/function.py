6403c2f8b70181a332b876a967892ca7b35024c5
Boris Koenig <Boris.Koenig@docc.techstarter.de>
2023-10-05 20:17:39 +0200

    refactor into separate getter

--- lambda/function.py
+++ lambda/function.py
@@ -9,23 +9,28 @@ logger.setLevel(logging.INFO)
 DEFAULT_OPERATION = 'add'
 DEFAULT_VALUE='1'
 
-DEFAULT_FORMAT='DEC' # dezimal
+DEFAULT_FORMAT='DEC' # decimal
 
-#  entry point 
-def lambda_handler(event, context):
+# helper function to get a variable
+def getVar(event, var, default):
     body = event.get('body')
     if body:
         requestBody = json.loads(body)
     else:
         requestBody = event
-        
-    x = int(requestBody.get('x', DEFAULT_VALUE))
-    y = int(requestBody.get('y', DEFAULT_VALUE))
+    return int(requestBody.get(var, default))
+    
+
+#  entry point 
+def lambda_handler(event, context):
+    x = getVar(event,'y',DEFAULT_VALUE)
+    y = getVar(event,'y',DEFAULT_VALUE)
+
 
     logger.info('Parameters: %s/%s', x, y)
     
-    operation = requestBody.get('operation',DEFAULT_OPERATION)
-    output = requestBody.get('format', DEFAULT_FORMAT)
+    operation = getVar('operation',DEFAULT_OPERATION)
+    output = getVar('format', DEFAULT_FORMAT)
 
     (result, status) = calculate(operation,x,y)
     logger.info('Result of %s %s %s = %s ', x, operation, y, result)
@@ -38,7 +43,7 @@ def lambda_handler(event, context):
         case 'OCT':
             result = oct(result)
         case _:
-            pass
+            logger.info('Unknown Output Format requested:',operation)
 
 
     return {
